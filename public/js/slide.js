e3a3b690d9a52f04902e2160e647e0fc66830cf4
tientcdtk5 <125197106+tientcdtk5@users.noreply.github.com>
2023-06-09 18:47:34 +0700

    Update slide.js

--- public/js/slide.js
+++ public/js/slide.js
@@ -1,3 +1,4 @@
+// slide first
 let slideIndex = 0;
 showSlides();
 
@@ -11,4 +12,25 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+// slide review
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  direction: getDirection(),
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+    },
+  },
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+  return direction;
 }
